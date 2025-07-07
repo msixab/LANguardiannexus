dc5f4c4592a33b34d582aa86ed9afc5070eaffe0
msixab <abdallahbsk123@gmail.com>
2025-07-07 12:17:21 +0100

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+},
+ build: {
+    rollupOptions: {
+      external: ['/src/main.tex'], // Explicitly allow this file
+    
+  },
+  },
+  plugins: [
+    react(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
